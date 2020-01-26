e619f0d7d55767340bed91e6988b70dd10667b65
Melvin <MelvinThreethousand@gmail.com>
2020-01-25 20:57:39 -0500

    Let projectiles shot by a player damage pvp logged players

--- src/com/Melvin3000/AntiPVPLog/EntityDamageByEntity.java
+++ src/com/Melvin3000/AntiPVPLog/EntityDamageByEntity.java
@@ -1,11 +1,23 @@
 package com.Melvin3000.AntiPVPLog;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class EntityDamageByEntity implements Listener {
+
+	private final static HashSet<EntityType> projectiles = new HashSet<EntityType>(Arrays.asList(
+			EntityType.ARROW,
+			EntityType.SPECTRAL_ARROW,
+			EntityType.TRIDENT
+	));
 
 	@EventHandler
 	public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
@@ -13,11 +25,20 @@ public class EntityDamageByEntity implements Listener {
 		/* Disable non player damage to offline players (Primarily stops wolves) */
 		if (AntiPVPLog.dummySkeletons.containsKey(event.getEntity().getUniqueId())) {
 
-			if (!(event.getDamager() instanceof Player)) {
-				event.setCancelled(true);
+			boolean cancel = true;
+			Entity damager = event.getDamager();
+			if (damager instanceof Player) {
+				cancel = false;
 			}
-		}
 
+			else if (projectiles.contains(damager.getType())) {
+				Projectile projectile = (Projectile) damager;
+				if (projectile.getShooter() instanceof Player) {
+					cancel = false;
+				}
+			}
+			event.setCancelled(cancel);
+		}
 	}
 
 }
